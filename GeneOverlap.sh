4777fb6a9c42c4c738c632acb0d59332fa81f333
MaryamEO <92852634+MaryamEO@users.noreply.github.com>
2026-04-02 19:09:03 +0200

    Update GeneOverlap.sh

--- GeneOverlap.sh
+++ GeneOverlap.sh
@@ -2,11 +2,11 @@
 
 phenotype=("Epilepsy" "GGE")
 
-subnetworks="/mnt/isilon/projects/isbsequencing/epi_rare/net_06032025/cluster/"
+subnetworks="/cluster/"
 
 for v in "${phenotype[@]}"; do
     echo "phenotype: $v"
-    ls ${subnetworks}*.tsv | parallel -j5  Rscript /mnt/isilon/projects/isbsequencing/epi_rare/network_29092025/GeneOverlap.R {} ${v}
+    ls ${subnetworks}*.tsv | parallel -j5  Rscript GeneOverlap.R {} ${v}
 done
 
 
